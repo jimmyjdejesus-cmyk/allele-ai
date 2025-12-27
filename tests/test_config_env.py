39140d0a2aa146b6176c95b00d8db36309d51eda
Jimmy-Dejesus <jimmy@bravetto.com>
2025-12-27 17:52:54 -0500

    test(ci): make env tests robust and memory bench less strict; fix ml_analytics imports; make tests package importable

--- tests/test_config_env.py
+++ tests/test_config_env.py
@@ -5,15 +5,38 @@ def test_env_override_agent_model():
     # Set env var to change AGENT model name
     os.environ["AGENT__MODEL_NAME"] = "test-model-env"
 
-    # Recreate settings to pick up environment change
-    from phylogenic.config import AlleleSettings
+    # Reload config module so runtime settings are recreated from env
+    import importlib
 
-    config = AlleleSettings()
+    import phylogenic.config as _config
 
-    assert config.agent.model_name == "test-model-env"
+    importlib.reload(_config)
+
+    from phylogenic.config import settings as module_settings
+
+    assert module_settings.agent.model_name == "test-model-env"
 
     # Cleanup
     del os.environ["AGENT__MODEL_NAME"]
+
+
+def test_env_override_evolution_immutable_and_hpc_flags():
+    os.environ["EVOLUTION__IMMUTABLE_EVOLUTION"] = "true"
+    os.environ["EVOLUTION__HPC_MODE"] = "false"
+
+    import importlib
+
+    import phylogenic.config as _config
+
+    importlib.reload(_config)
+
+    from phylogenic.config import settings as module_settings
+
+    assert module_settings.evolution.immutable_evolution is True
+    assert module_settings.evolution.hpc_mode is False
+
+    del os.environ["EVOLUTION__IMMUTABLE_EVOLUTION"]
+    del os.environ["EVOLUTION__HPC_MODE"]
 
 
 def test_env_override_evolution_immutable_and_hpc_flags():
